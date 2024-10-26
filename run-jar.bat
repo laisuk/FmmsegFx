abaf9e374bf1b9a2ac4dbe3d649c5a843c2e194f
laisuk <laisuk@yahoo.com>
2024-10-26 23:42:19 +0800

    Update JavaFX

--- run-jar.bat
+++ run-jar.bat
@@ -1,3 +1,3 @@
 @echo off
 rem java -jar "-Dfile.encoding=UTF-8" --module-path "C:\Java\javafx-sdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml build/libs/fmmsegfx-1.0-SNAPSHOT.jar
-C:\Users\user\.jdks\graalvm-jdk-21.0.5+9.1\bin\java.exe -jar "-Dfile.encoding=UTF-8" --module-path "C:\Java\javafx-sdk-23.0.1\lib" --add-modules javafx.controls,javafx.fxml build/libs/fmmsegfx-1.0-SNAPSHOT.jar
+C:\Users\user\.jdks\graalvm-jdk-21.0.5\bin\java.exe -jar "-Dfile.encoding=UTF-8" --module-path "C:\Java\javafx-sdk-23.0.1\lib" --add-modules javafx.controls,javafx.fxml build/libs/fmmsegfx-1.0-SNAPSHOT.jar
