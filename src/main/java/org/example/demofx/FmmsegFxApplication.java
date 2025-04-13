f0da2fe82ed204adc62c0b4dc08c63cc19045f19
laisuk <laisuk@yahoo.com>
2025-04-13 13:55:11 +0800

    Remove BOM from opened text file

--- src/main/java/org/example/demofx/FmmsegFxApplication.java
+++ src/main/java/org/example/demofx/FmmsegFxApplication.java
@@ -11,7 +11,7 @@ public class FmmsegFxApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FmmsegFxApplication.class.getResource("fmmsegfx-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 960, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1020, 700);
         stage.setTitle("FmmsegFX 1.0");
         stage.setScene(scene);
         stage.show();
