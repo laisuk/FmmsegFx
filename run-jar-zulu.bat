eb6ebabdb750ba408a8a6380ab01564d5c9b5b04
laisuk <laisuk@yahoo.com>
2025-04-29 06:53:38 +0800

    Update CAPI to Win7 x64 compatible

--- run-jar-zulu.bat
+++ run-jar-zulu.bat
@@ -1,3 +1,2 @@
 @echo off
-C:\Java\zulu23.32.11-ca-fx-jdk23.0.2-win_x64\bin\java.exe -jar "-Dfile.encoding=UTF-8" --module-path "C:\Java\richtextfx" --add-modules org.fxmisc.richtext build/libs/fmmsegfx-2.0-SNAPSHOT.jar
-REM C:\Java\zulu24.28.85-ca-fx-jdk24.0.0-win_x64\bin\java.exe -jar "-Dfile.encoding=UTF-8" build/libs/fmmsegfx-1.0-SNAPSHOT.jar
+C:\Java\zulu21.42.19-ca-fx-jdk21.0.7-win_x64\bin\java.exe -jar "-Dfile.encoding=UTF-8" --module-path "C:\Java\richtextfx" --add-modules org.fxmisc.richtext build/libs/fmmsegfx-2.0-SNAPSHOT.jar
